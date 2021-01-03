95d7a1c0c49838cf783caca36fb316937427ba1d
Cillian Reilly <cillianreilly@yahoo.com>
2021-01-03 04:12:15 +0000

    Moved mic to seperate process to avoid blocking

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 echo "Starting paradise..."
-$QHOME/l32/q run.q -p 5000 -cli 0 -mic 0 -ml 1 >/dev/null >logs/paradise.log 2>&1 &
+$QHOME/l32/q run.q -p 5000 -cli 0 -ml 1 2>&1 >logs/paradise.log &
+
+echo "Starting mic..."
+$QHOME/l32/q mic.q -cli 0 2>&1 >logs/paradise.log &
 
 echo "Starting ngrok server..."
-ngrok http -subdomain=paradise 5000 2>&1 >/dev/null &
+/home/pi/Downloads/ngrok http -subdomain=paradise 5000 2>&1 >/dev/null &
 
 echo "Started"
 exit 0
