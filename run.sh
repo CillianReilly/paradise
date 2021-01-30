7fa091724e877fb355f8552d8cb78b4c237d8798
Cillian Reilly <cillianreilly@yahoo.com>
2021-01-30 06:57:41 +0000

    "Update mic log location"

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "Starting paradise..."
 $QHOME/l32/q run.q -p 5000 -cli 0 -ml 1 2>&1 >logs/paradise.log &
 
 echo "Starting mic..."
-$QHOME/l32/q mic.q -cli 0 2>&1 >logs/paradise.log &
+$QHOME/l32/q mic.q -cli 0 2>&1 >logs/mic.log &
 
 echo "Starting ngrok server..."
 /home/pi/Downloads/ngrok http -subdomain=paradise 5000 2>&1 >/dev/null &
