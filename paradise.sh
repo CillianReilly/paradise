9926fca780c52e77233e41fba0a89589562e15a5
Cillian Reilly <cillianreilly@yahoo.com>
2021-03-20 07:37:26 +0000

    Rename and symlink

--- paradise.sh
+++ paradise.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-echo "Starting paradise..."
-$QHOME/l32/q paradise.q -p 5000 -cli 0 -ml 1 2>&1 >logs/paradise.log &
-
-echo "Starting mic..."
-$QHOME/l32/q mic.q -cli 0 2>&1 >logs/mic.log &
-
-echo "Starting ngrok server..."
-$NGROK_HOME/ngrok http -subdomain=paradise 5000 2>&1 >/dev/null &
-
-echo "Started"
-exit 0
+start.sh
