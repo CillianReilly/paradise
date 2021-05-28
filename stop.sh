4e15875b8d0ed0931e8c2490b17e68ec1dc66687
Cillian Reilly <cillianreilly@yahoo.com>
2021-05-28 14:24:03 +0100

    Don't kill if no results

--- stop.sh
+++ stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Stopping mic and ngrok..."
-ps -ef | grep "mic.q\|ngrok" grep -v grep | awk '{print $2}' | xargs kill -9 2>/dev/null
+ps -ef | grep "mic.q\|ngrok" grep -v grep | awk '{print $2}' | xargs --no-run-if-empty kill -9 2>/dev/null
 
 echo "Stopping Paradise..."
-$QHOME/l32/q rrc.q -port 6000 -cmd "exit 0" -q
+$QHOME/l32/q rrc.q -port 5000 -cmd "exit 0" -q
 
 echo "Paradise shutdown completed"
