bb735de58c084a153333488b75f80f4f5d7bb674
Cillian Reilly <cillianreilly@yahoo.com>
2021-06-06 02:09:19 +0100

    Merge rpl and ctl

--- paradiseRpl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Setting environment variables from paradise.cfg..."
-PORT=$(grep PORT paradise.cfg | cut -d "=" -f2)
-
-rlwrap -r $QHOME/l32/q rpl.q -port $PORT -host localhost
--- paradisectl.sh
+++ paradisectl.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-if [ -z $1 ];then
-	echo "Usage: ./$(basename $0) cmd"
-	exit 1
-fi
-
 if [ $# -ge 2 ];then
 	echo "Commands containing a space must be wrapped in quotes"
 	echo "Usage: ./$(basename $0) \"$@\""
 	exit 1
 fi
 
-echo "Setting environment variables from paradise.cfg..."
 PORT=$(grep PORT paradise.cfg | cut -d "=" -f2)
 
-$QHOME/l32/q rrc.q -port $PORT -cmd "paradise\"$1\"" -q
+if [ $# -eq 0 ];then
+	rlwrap -r $QHOME/l32/q rpl.q -port $PORT
+else
+	$QHOME/l32/q rrc.q -port $PORT -cmd "paradise\"$1\"" -q
+fi
