0704409f05f4260a92ecb33797e952774ebcbe6a
Cillian Reilly <cillianreilly@yahoo.com>
2021-08-28 02:37:56 +0100

    Use :port format

--- stop.sh
+++ stop.sh
@@ -7,6 +7,6 @@ echo "Stopping mic and ngrok..."
 ps -ef | grep "mic.q\|ngrok" | grep $PORT | awk '{print $2}' | xargs --no-run-if-empty kill -9
 
 echo "Stopping Paradise..."
-$QHOME/l32/q rrc.q -port $PORT -cmd "exit 0" -q
+$QHOME/l32/q rrc.q :$PORT -cmd "exit 0" -q
 
 echo "Paradise shutdown completed"
